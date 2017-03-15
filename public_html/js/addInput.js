df11cce1b4675eb8cb6d12aa7a1fcadf2b29061e
badde-media <github@badde.media>
2017-03-15 02:02:38 +0100

    Add files via upload

--- public_html/js/addInput.js
+++ public_html/js/addInput.js
@@ -1,13 +1,13 @@
     
-var counter = 2;
-var limit = 4;
+var counter = 3;
+var limit = 5;
 function addInput(divName){
      if (counter === limit)  {
-          alert("Max 4 Kriterien in der Light Version - Zu Pro Upgraden");
+          alert("Max " + counter + " Kriterien - Zur Pro Version");
      }
      else {
           var newdiv = document.createElement('div');
-          newdiv.innerHTML = " <input type='text' id='skriterium"+counter+"'  name='myInputs[]' placeholder='Kriterium eingeben'>";
+          newdiv.innerHTML = " <input type='text' id='sing_crit"+counter+"'  name='myInputs[]' placeholder='Kriterium eingeben'>";
           document.getElementById(divName).appendChild(newdiv);
           counter++;
      }
