73c2262d98bc8aa00c3338a241033daaf00d93d9
badde-media <github@badde.media>
2017-04-04 04:52:18 +0200

    01.04.
    Version Katja 01.04.17

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,1 +1,7 @@
-$(document).foundation()
+ $(document).foundation();
+$(document).ready(function(){
+    $(".skala > div").on("click", function(){
+        $(".skala > div").removeClass("selected");
+        $(this).addClass("selected");
+    });
+}); 
