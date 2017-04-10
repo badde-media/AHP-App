be2924aeb15e0115c57b2459b0cfdd73993d8f6f
badde-media <github@badde.media>
2017-04-11 01:49:03 +0200

    Fast fertig
    Alles außer Reult_L fertig. ToDo: Gesamtwichtung berechnen und ausgeben

--- public_html/js/addInput.js
+++ public_html/js/addInput.js
@@ -1,14 +1,31 @@
-    
-var counter = 3;
-var limit = 4;
-function addInput(divName){
-     if (counter === limit)  {
-          alert("Max 4 Kriterien in der Light Version - Zu Pro Upgraden");
-     }
-     else {
-          var newdiv = document.createElement('div');
-          newdiv.innerHTML = " <input type='text' id='skriterium"+counter+"'  name='myInputs[]' placeholder='Kriterium eingeben'>";
-          document.getElementById(divName).appendChild(newdiv);
-          counter++;
-     }
+
+function addInput(varname, divName, typ){
+    switch (typ)
+    {
+        case "Kriterium":
+            if (kriterienAnzahl === klimit)  {
+                 alert("Max " + klimit + " Kriterien in der Light Version - Zu Pro Upgraden");
+                 return(kriterienAnzahl);
+            }
+            else {
+                 kriterienAnzahl++;
+            }
+            var newdiv = document.createElement('div');
+            newdiv.innerHTML = " <input type='text' id='"+varname+kriterienAnzahl+"'  name='myInputs[]' placeholder='"+typ+" eintragen'>";
+            document.getElementById(divName).appendChild(newdiv);
+            return(kriterienAnzahl);
+            
+        case "Lösung":
+            if (loesungenAnzahl === llimit)  {
+                 alert("Max " + llimit + " Lösungen in der Light Version - Zu Pro Upgraden");
+                 return(loesungenAnzahl);
+            }
+            else {
+                 loesungenAnzahl++;
+            }
+            var newdiv = document.createElement('div');
+            newdiv.innerHTML = " <input type='text' id='"+varname+loesungenAnzahl+"'  name='myInputs[]' placeholder='"+typ+" eintragen'>";
+            document.getElementById(divName).appendChild(newdiv);
+            return(loesungenAnzahl);
+    }
 } 
